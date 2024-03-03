37d59bca3ea7ff3f5c8065fd7cd2372af00d406d
Rainer Strauss <rainer@strauss-web.ch>
2024-03-03 19:13:16 +0000

    s2_3_1
    Tontraeger mit UML

---
+++ s2_3_1_tontraeger/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        new Tontraeger();
+        new Band();
+        new Stueck();
+        new Musiker();
+        new Album();
+
+    }
+}
